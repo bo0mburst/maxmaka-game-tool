22ce7889341c92c6b5f5b38aa9a869ac4ab24b1c
Billy Sillano <billysillano@gmail.com>
2023-02-12 10:50:06 +0800

    Update theme and add functions

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ window.MAXMAKA = {
 	isChatListenerActive: false,
 	eligibles: [],
 	leaderboard: {},
+	channel: 'splinterlandstv',
 };
 
 const STATE_KEY =  'MAXMAKA';
@@ -19,36 +20,26 @@ const clearLeaderboard = document.getElementById('clear-leaderboard');
 const buttonEligibleWinner = document.getElementById('button-eligible-winner');
 const buttonSave = document.getElementById('button-save');
 
+const channelEl = document.getElementById('channel');
+const channelBtn = document.getElementById('channel-btn');
+
+const searchName = document.getElementById('search-name');
+
 const winnerModal = new bootstrap.Modal(document.getElementById('winner-modal'), {
 	backdrop: 'static',
   keyboard: false
 });
 
-
 clearEligibles.addEventListener('click', clearEligiblesList);
 clearLeaderboard.addEventListener('click', clearLeaderboardList);
-keywordButton.addEventListener('mousedown', toggleKeyWordVisibility);
-keywordButton.addEventListener('mouseup', toggleKeyWordVisibility);
+keywordButton.addEventListener('click', toggleKeyWordVisibility);
 buttonEligibleWinner.addEventListener('click', pickEligibleWinner);
 buttonSave.addEventListener('click', saveToLeaderboard);
-
+channelBtn.addEventListener('click', updateChannel);
+searchName.addEventListener('input', filterLeaderboard);
 
 // load save state 
 initialLoad();
-
-// twitch client
-const client = new tmi.Client({
-  connetion: {
-    secure: true,
-    reconnect: true,
-  },
-	channels: [ 'splinterlandstv' ],
-});
-client.connect().catch(console.error);
-client.on('message', (channel, tags, message, self) => {
-	logMessages(tags, message);
-	logEligibles(tags, message);
-});
 
 
 // FUNCTIONS
@@ -56,17 +47,70 @@ client.on('message', (channel, tags, message, self) => {
 function initialLoad() {
 	const state = getState();
 	if (state) {
-		window.MAXMAKA.leaderboard = state;
-		renderLeaderBoard(state);
+		window.MAXMAKA = {
+			isChatListenerActive: false,
+			eligibles: state.eligibles || [],
+			leaderboard: state.leaderboard || {},
+			channel: state.channel || 'splinterlandstv',
+		}
 	}
+
+	renderLeaderBoard();
+	renderEligibles();
+
+	const channel = window.MAXMAKA.channel;
+	channelEl.innerText = channel;
+	// twitch client
+	const client = new tmi.Client({
+		connetion: {
+			secure: true,
+			reconnect: true,
+		},
+		channels: [ channel ],
+	});
+	client.connect().catch(console.error);
+	client.on('message', (channel, tags, message, self) => {
+		logMessages(tags, message);
+		logEligibles(tags, message);
+	});
 }
 
-function saveState(state = []) {
-	window.localStorage.setItem(STATE_KEY, JSON.stringify(state));
+function saveState() {
+	window.localStorage.setItem(STATE_KEY, JSON.stringify(window.MAXMAKA));
 }
 
 function getState() {
 	return JSON.parse(window.localStorage.getItem(STATE_KEY));
+}
+
+function filterLeaderboard() {
+	const filter = searchName.value.toLocaleLowerCase();
+	const items = leaderboardBox.querySelectorAll('li');
+
+	console.log(filter, items)
+	
+	items.forEach((item) => {
+    let name = item.id;
+    if (String(name.toLocaleLowerCase()).startsWith(filter)) {
+      item.classList.remove('d-none');
+    } else {
+      item.classList.add('d-none');
+    }
+	})
+}
+
+function updateChannel() {
+	const channel = prompt('Change channel', window.MAXMAKA.channel);
+	console.log(channel);
+	window.MAXMAKA = {
+		isChatListenerActive: false,
+		eligibles: [],
+		leaderboard: {},
+		channel: channel
+	};
+	console.log(window.MAXMAKA)
+	saveState()
+	location.reload(); 
 }
 
 function clearLeaderboardList () {
@@ -79,17 +123,29 @@ function clearLeaderboardList () {
 }
 
 function saveToLeaderboard () {
-	if (!confirm('This will clear the eligible list')) return;
+	const eligibles = window.MAXMAKA.eligibles;
+	
+	if (!eligibles || !eligibles.length) return; 
 
 	const lb = window.MAXMAKA.leaderboard;
-	const eligibles = window.MAXMAKA.eligibles;
+	let points;
+	do{
+		points = prompt('Enter number of points to add. 0-100', 1);
+	} while (isNaN(points));
+
+
 	eligibles.forEach(user => {
 		if (!lb[user]) lb[user] = 0;
-		lb[user] += 1;
+		lb[user] += Number(points);
 	});
 
-	// sort
+	updateLeaderBoard();
+}
 
+function updateLeaderBoard() {
+	const lb = window.MAXMAKA.leaderboard;
+
+	// sort
 	const sortedLb = Object.keys(lb)
   .sort((key1, key2) => lb[key2] - lb[key1] )
   .reduce(
@@ -101,29 +157,34 @@ function saveToLeaderboard () {
   );
 
 	// save to storage
-	saveState(sortedLb);
+	window.MAXMAKA.leaderboard = sortedLb;
+	saveState();
 
 	// render
-	
-	renderLeaderBoard(sortedLb);
-
-
-	clearEligiblesList();
+	renderLeaderBoard();
 }
 
-function renderLeaderBoard(sortedLb) {
-	if (!sortedLb) return;
+function renderLeaderBoard() {
+	const lb = window.MAXMAKA.leaderboard;
 	leaderboardBox.innerHTML =  '';
-
-	for (const user in sortedLb) {
+	
+	for (const user in lb) {
 		const li = document.createElement('li');
-		const nameEl = document.createElement('strong');
-		const scoreEl = document.createElement('span');
-		scoreEl.classList.add('badge', 'bg-secondary', 'ms-2');
-		scoreEl.innerText = sortedLb[user];
+		const nameEl = document.createElement('span');
 		nameEl.innerText = user;
 
+		const scoreEl = document.createElement('input');
+		scoreEl.setAttribute('type', 'number');
+		scoreEl.classList.add('text-warning', 'ms-2', 'float-end', 'border-0', 'bg-transparent', 'text-end');
+		scoreEl.style.width = '50px';
+		scoreEl.value = lb[user];
+		scoreEl.addEventListener('blur', function (e) {
+			lb[user] = e.target.value;
+			updateLeaderBoard();
+		});
+
 		li.classList.add('list-group-item', 'small', 'text-body');
+		li.setAttribute('id', user);
 		li.append(nameEl);
 		li.append(scoreEl);
 
@@ -161,7 +222,8 @@ function toggleKeyWordVisibility() {
 }
 
 function clearEligiblesList () {
-	eligiblesBox.innerHTML = '';
+	if(!confirm('This will clear eligible list.')) return;
+	eligiblesBox.innerHTML = '<div class="text-muted text-center mt-3">List will show here</div>';
 	window.MAXMAKA.eligibles = [];
 }
 
@@ -170,16 +232,18 @@ function logMessages (tags, message) {
 	const displayName = tags['display-name'];
 	
 	const li = document.createElement('li');
-	const nameEl = document.createElement('strong');
+	const nameEl = document.createElement('span');
 	const messageEl = document.createElement('span');
+	nameEl.style.color = tags.color;
 	nameEl.innerText = displayName;
+	messageEl.classList.add('text-muted');
 	messageEl.innerText =  `: ${message}`;
 
 	li.classList.add('list-group-item', 'small', 'text-body');
 	li.append(nameEl);
 	li.append(messageEl);
 
-	if (twitchChat.childNodes.length > 20) {
+	if (twitchChat.childNodes.length > 100) {
 		twitchChat.removeChild(twitchChat.getElementsByTagName('li')[0]);
 	}
 
@@ -192,24 +256,32 @@ function logEligibles (tags, message) {
 	const msg = message.toLocaleLowerCase();
 	const key = String(keywordInput.value).toLocaleLowerCase();
 	// eligibles
+	const eligibles = window.MAXMAKA?.eligibles || [];
 	if (key && msg.includes(key)) {
 		const displayName = tags['display-name'];
-		const eligibles = window.MAXMAKA.eligibles || [];
 		if (eligibles.includes(displayName)) return 
 
 		eligibles.push(displayName);
-	
-		eligiblesBox.innerHTML = '';
-	
-		eligibles.forEach(user => {
-			const li = document.createElement('li');
-			const nameEl = document.createElement('strong');
-			nameEl.innerText = user;
-	
-			li.classList.add('list-group-item', 'small', 'text-success');
-			li.append(nameEl);
-	
-			eligiblesBox.append(li);
-		});
+		window.MAXMAKA.eligibles = eligibles;
+		renderEligibles();
 	}
+}
+
+function renderEligibles() {
+	const eligibles = [...window.MAXMAKA.eligibles] || [];
+
+	eligiblesBox.innerHTML = '';
+	
+	eligibles.forEach(user => {
+		const li = document.createElement('li');
+		const nameEl = document.createElement('span');
+		nameEl.innerText = user;
+
+		li.classList.add('list-group-item', 'small', 'text-success');
+		li.append(nameEl);
+
+		eligiblesBox.append(li);
+	});
+
+	saveState();
 }
